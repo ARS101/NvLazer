609fcbe9feafe9fc61f07e715f6ff467d6011d92
ARS101 <D3F4U1T_ARS@proton.me>
2023-04-23 17:05:14 +0330

    Configuring basic completion functionalities:
    Added and configured:
    hrsh7th/nvim-cmp
    hrsh7th/cmp-nvim-lsp
    L3MON4D3/LuaSnip

--- init.lua
+++ init.lua
@@ -34,6 +34,16 @@ plugins = {
 			"neovim/nvim-lspconfig",
 			"williamboman/mason.nvim",
 		}
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{
+				"L3MON4D3/LuaSnip",
+				version = "1.*",
+			},
+			"hrsh7th/cmp-nvim-lsp",
+		}
 	}
 }
 
@@ -54,6 +64,29 @@ require("nvim-treesitter.configs").setup(
 	}
 )
 
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({select = true}),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, { { name = "buffer" }, })
+})
+
 require("mason").setup()
 require("mason-lspconfig").setup(
 	{
@@ -65,3 +98,5 @@ require("mason-lspconfig").setup(
 local lspconfig = require("lspconfig")
 lspconfig.lua_ls.setup({ settings = { Lua = { } } })
 lspconfig.pylsp.setup({ setting = { pylsp = { } } })
+
+
