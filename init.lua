e764690ef767128743d013a1425f82f38352ed2e
ARS101 <D3F4U1T_ARS@proton.me>
2023-04-25 18:14:52 +0330

    Doing a lot of stuff
    Fixing the bug that annoys when you open a lua file
    Adding LuaSnip for snippet completion
    Adding barbar.nvim for tab support
    Line number, 4 space tabs and more quality of life changes
    Adding pylsp plugin support
    Adding some useful keybindings
    Adding the missing explanation comments
    And formatting the code with 4 space indents

--- init.lua
+++ init.lua
@@ -3,14 +3,14 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Checking if lazy is installed in lazypath
 if not vim.loop.fs_stat(lazypath) then
-	print("Installing lazy for the first time.")
-	vim.fn.system({"git",
-	"clone",
-	"--filter=blob:none",
-	"https://github.com/folke/lazy.nvim.git",
-	"--branch=stable",
-	lazypath,})
-	print("Installation complete.")
+    print("Installing lazy for the first time.")
+    vim.fn.system({ "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath, })
+    print("Installation complete.")
 end
 
 -- Adding lazypath to runtimepath (rtp)
@@ -19,72 +19,105 @@ vim.opt.rtp:prepend(lazypath)
 -- Setting leader key
 vim.g.mapleader = " "
 
+-- Set line number and relative line number
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Set to 4 space tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- No line wrapping
+vim.opt.wrap = false
+
+vim.opt.scrolloff = 8
+
+-- Update time to save the file
+vim.opt.updatetime = 50
+
 -- List of plugins to be installed
 local plugins = {
 
-	-- Treesitter provides basic functionalities such as highlighting
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"
-	},
+    -- Treesitter provides basic functionalities such as highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
 
-	-- Telescope is a powerful fuzzy finder
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = { 'nvim-lua/plenary.nvim' },
-	},
+    -- Telescope is a powerful fuzzy finder
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
 
-	-- Onedark theme
-	{"navarasu/onedark.nvim"},
+    -- Onedark theme
+    { "navarasu/onedark.nvim" },
 
-	{
-		-- Bridges mason.nvim with the lspconfig plugins
-		-- making it easier to use both plugins together. 
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			-- Configurations for the Nvim LSP client
-			-- Used to pass configuration to lsp servers
-			"neovim/nvim-lspconfig",
+    {
+        -- Bridges mason.nvim with the lspconfig plugins
+        -- making it easier to use both plugins together.
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            -- Configurations for the Nvim LSP client
+            -- Used to pass configuration to lsp servers
+            "neovim/nvim-lspconfig",
 
-			-- Package manager used to install
-			-- LSP servers, DAP servers, linters, and formatters. 
-			"williamboman/mason.nvim",
-		}
-	},
+            -- Package manager used to install
+            -- LSP servers, DAP servers, linters, and formatters.
+            "williamboman/mason.nvim",
+        }
+    },
 
-	{
-		-- Nvim completion engine
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			-- Critical for nvim completion functionalities
-			"hrsh7th/cmp-nvim-lsp",
-		}
-	},
+    {
+        -- Nvim completion engine
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            -- Critical for nvim completion functionalities
+            "hrsh7th/cmp-nvim-lsp",
+            -- Snippet engine required for completion to work
+            { "L3MON4D3/LuaSnip", version = "1.*", },
+        }
+    },
 
-	{ "folke/neodev.nvim" },
+    -- Signature help, docs and completion for the neovim lua API. 
+    { "folke/neodev.nvim" },
 
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = { "nvim-tree/nvim-web-devicons" },
-	},
+    -- A blazing fast and easy to configure neovim statusline plugin
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = { "nvim-tree/nvim-web-devicons" },
+    },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		opts = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("nvim-tree").setup {}
-		end,
-	},
+    -- A file explorer for neovim
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        opts = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
 
-	{
-		's1n7ax/nvim-terminal',
-		config = function()
-        		vim.o.hidden = true
-			require('nvim-terminal').setup()
-		end,
-	},
+    -- A togglable terminal plugin for neovim
+    {
+        's1n7ax/nvim-terminal',
+        config = function()
+            vim.o.hidden = true
+            require('nvim-terminal').setup()
+        end,
+    },
+
+    -- Editor tab plugin for showing opened files
+    {
+        'romgrk/barbar.nvim',
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        init = function() vim.g.barbar_auto_setup = false end,
+        version = '^1.0.0',
+    },
 }
 
 
@@ -97,44 +130,48 @@ require("onedark").setup {
 require("onedark").load()
 
 require("nvim-treesitter.configs").setup(
-	{
-		-- Put your languages here to to enable syntax highlighting for it
-		-- Check here: https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-		ensure_installed = { "lua", "python" },
-
-		auto_install = true,
-		highlight = { enable = true },
-	}
+    {
+        -- Put your languages here to to enable syntax highlighting for it
+        -- Check here: https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+        ensure_installed = { "lua", "python" },
+        auto_install = true,
+        highlight = { enable = true },
+    }
 )
 
 local cmp = require("cmp")
 
 cmp.setup({
-	window = {
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({select = true}),
-	}),
-	sources = cmp.config.sources({
-		-- Critical for nvim completion functionalities
-		{ name = "nvim_lsp" },
-	})
+    snippet = {
+        -- Setting up snippet completion
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    window = {
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        -- Critical for nvim completion functionalities
+        { name = "nvim_lsp" },
+    })
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup(
-	{
-		automatic_installation = true,
-
-		-- LSPs to install
-		-- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
-		ensure_installed = { "lua_ls", "pylsp" },
-	}
+    {
+        automatic_installation = true,
+        -- LSPs to install
+        -- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
+        ensure_installed = { "lua_ls", "pylsp" },
+    }
 )
 
 require("neodev").setup({})
@@ -142,15 +179,56 @@ require("neodev").setup({})
 local lspconfig = require("lspconfig")
 
 -- Lua LSP configurations
-lspconfig.lua_ls.setup({ settings = { Lua = { completion = { callSnippet = "Replace" } } } })
+lspconfig.lua_ls.setup({
+    settings = {
+        Lua = {
+            -- Fix the annoyance of:
+            -- Do you need to configure your work environment as `luv`?
+            workspace = { checkThirdParty = false }
+        }
+    }
+})
 
 -- Python LSP configurations
-lspconfig.pylsp.setup({ setting = { pylsp = { } } })
+lspconfig.pylsp.setup({
+    setting = {
+        pylsp = {
+            plugins = {
+                pylsp_mypy = { enabled = true },
+                pylsp_black = { enabled = true },
+                pylsp_isort = { enabled = true },
+                pycodestyle = { enabled = true, maxLineLength = 88 },
+            }
+        }
+    }
+})
 
+vim.lsp.set_log_level("debug")
+
+-- Telescope keybindings
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-require('lualine').setup()
+require('lualine').setup({})
+
+-- Formats the current file
+vim.keymap.set("n", "<C-f>", function()
+    vim.lsp.buf.format({ async = true })
+    print("Formatted")
+end, { silent = true })
+
+-- Move the selected text up and down (with tab support)
+vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv")   -- Alt + Up Arrow Key
+vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv") -- Alt + Down Arrow Key
+
+-- Keeps the selected search regex in the middle
+-- when you go to next or previous occurance
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Copies the selected line/text to your clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
